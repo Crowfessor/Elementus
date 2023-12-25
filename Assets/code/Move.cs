e500a233330a233d7d0f29d3871287aa75e616ce
NamlessCoder <117368304+NamlessCoder@users.noreply.github.com>
2023-12-25 17:12:07 +0300

    aa

--- Assets/code/Move.cs
+++ Assets/code/Move.cs
@@ -28,10 +28,13 @@ public class hareket : MonoBehaviour
     public float wallHopForce;
     public float wallJumpForce;
 <<<<<<< HEAD
+<<<<<<< HEAD
     public float turnTimer;
     public float turnTimerSet;
    
 
+=======
+>>>>>>> parent of 304cec2 (bug fix down animation)
 =======
 >>>>>>> parent of 304cec2 (bug fix down animation)
 
