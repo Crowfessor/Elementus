f6f4e3da144e07ab8416772f0f29e1ef6518e163
Jaden Winzlow <jadenwinzlow@gmail.com>
2023-12-25 18:27:23 +0300

    Yedek dosyalar

---
+++ Assets/code/Move.cs
@@ -0,0 +1,205 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Animations;
+using UnityEngine;
+
+public class hareket : MonoBehaviour
+{
+    Rigidbody2D rb;
+    Animator ar;
+    BoxCollider2D bc;
+
+    public LayerMask IsGround;
+    public LayerMask Walllayer;
+
+    private int PDirection = 1;
+    private int Health = 100;
+
+    public float horizontal;
+    public float speed = 500;
+    public float jumpforce = 5;
+    public float checkRadius = 0.27f;
+    public float extrajump;
+    public float extrajumpValue = 1;
+    public float wallslidespeed;
+    public float jumpheight;
+    public float ForceInAir;
+    public float airDragMultiplier;
+    public float wallHopForce;
+    public float wallJumpForce;
+
+    public bool canMove;
+
+    public Vector2 wallHopDirection;
+    public Vector2 wallJumpDirection;
+   
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        ar = GetComponent<Animator>();
+        bc = GetComponent<BoxCollider2D>();
+        wallHopDirection.Normalize();
+        wallJumpDirection.Normalize();
+
+        ControlMove(true);
+
+
+
+    }
+
+
+    void Update()
+    {
+
+      
+        Jump();
+        Flip();
+       
+
+    
+        ar.SetBool("Grounded", IsGrounded());
+
+
+
+        move();
+
+
+
+
+    }
+    public void ControlMove(bool can)
+    {
+        if (can)
+        {
+            canMove = true;
+        }
+        else
+        {
+            canMove = false;
+        }
+    }
+    private void FixedUpdate()
+    {
+
+        //move();
+
+        
+    }
+
+    private void move()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        if (IsGrounded() && canMove)
+        {
+            rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+            ar.SetBool("run", horizontal != 0);
+        }
+        else if(!IsGrounded() && !OnWall() && horizontal != 0)
+        {
+            Vector2 forceToAdd = new Vector2(ForceInAir * horizontal, 0);
+            rb.AddForce(forceToAdd);
+
+            if (Mathf.Abs(rb.velocity.x) > speed)
+            {
+                rb.velocity = new Vector2(speed * horizontal,rb.velocity.y);
+            }
+        }
+        else if(!IsGrounded() && !OnWall() && horizontal == 0)
+        {
+            rb.velocity = new Vector2 (rb.velocity.x * airDragMultiplier,rb.velocity.y);
+        }
+     
+
+
+        if (OnWall() && !IsGrounded() && rb.velocity.y < 0)
+        {
+            extrajump = extrajumpValue;
+            if (rb.velocity.y < -wallslidespeed)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, -wallslidespeed);
+                ar.SetBool("iswall", true);
+            }
+        }
+        else
+        {
+            
+            ar.SetBool("iswall", false);
+        }
+
+   
+        
+    }
+    private void Flip()
+    {
+        if (!OnWall() && rb.velocity.y <= 0)
+        {
+            if (horizontal > 0.01f)
+            {
+                transform.localScale = new Vector3(6, 6, 6);
+                PDirection = 1;
+            }
+            else if (horizontal < 0)
+            {
+                transform.localScale = new Vector3(-6, 6, 6);
+                PDirection = -1;
+            }
+        }
+    }
+    private void Jump()
+    {
+
+        if (IsGrounded())
+        {
+            extrajump = extrajumpValue;
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && extrajump > 0)
+        {
+            ar.SetBool("iswall", false);
+            if (!OnWall())
+            {
+                rb.velocity = new Vector2(rb.velocity.x, jumpforce);
+                
+            }
+            else if (!IsGrounded() && OnWall() && horizontal != 0)
+            {
+                
+                Vector2 forceToAdd = new Vector2(wallJumpDirection.x * wallJumpForce * -PDirection, wallJumpForce * wallJumpDirection.y);
+                rb.AddForce(forceToAdd, ForceMode2D.Impulse);
+            }
+            else if (!IsGrounded() && OnWall() && horizontal == 0)
+            {
+                
+                Vector2 forceToAdd = new Vector2(wallHopDirection.x * wallHopForce * -PDirection, wallHopForce * wallHopDirection.y);
+                rb.AddForce(forceToAdd, ForceMode2D.Impulse);
+            }
+
+            ar.SetTrigger("jump");
+           
+            extrajump--;
+
+
+        }
+        else if (Input.GetKeyUp(KeyCode.Space) && rb.velocity.y > 0)
+        {
+            rb.velocity = new Vector2(rb.velocity.x,rb.velocity.y * jumpheight);
+        }
+        
+    }
+    
+    public bool IsGrounded()
+    {
+        RaycastHit2D raycashit = Physics2D.BoxCast(bc.bounds.center, bc.bounds.size, 0, Vector2.down, 0.1f,IsGround);
+        return raycashit.collider != null;
+    }
+    public bool OnWall()
+    {
+        RaycastHit2D raycashit = Physics2D.BoxCast(bc.bounds.center, bc.bounds.size, 0,new Vector2(transform.localScale.x,0), 0.1f, Walllayer);
+        return raycashit.collider != null;
+    }
+    public void Healthchange(int HealthchangeValue)
+    {
+        Health += HealthchangeValue; 
+    }
+
+
+}
